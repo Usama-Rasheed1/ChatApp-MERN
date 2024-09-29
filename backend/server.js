43c0c500514798a90d3d7b1d0498058fa3505193
Usama Rasheed <usamara760@gmail.com>
2024-09-30 03:22:10 +0500

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/authRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -8,8 +10,9 @@ import userRoutes from "./routes/userRoutes.js";
 import connectDB from "./db/connectDB.js";
 import { app, server } from "./socket/socket.js";
 
-
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json());
@@ -18,6 +21,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(5000, () => {
   connectDB();
